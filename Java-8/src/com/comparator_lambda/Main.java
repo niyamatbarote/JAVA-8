d284d9cbf48b651b20838a7758a4a9b00a7b8f86
niyamat <niyamatbarote@gmail.com>
2026-01-03 20:14:51 +0530

    Done

--- Java-8/src/com/comparator_lambda/Main.java
+++ Java-8/src/com/comparator_lambda/Main.java
@@ -18,6 +18,7 @@ public class Main {
         System.out.println("Unsorted List: \n" + list);
         Collections.sort(list);
         System.out.println("Ascending Sorted List: \n"+list);
+        // Using Comparator Interface Without Making MyClass class
         Collections.sort(list, (a,b) -> b-a);
         System.out.println("Descending Sorted List: ");
         System.out.print(list);
